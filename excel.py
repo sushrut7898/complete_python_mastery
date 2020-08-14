cd10c6fe3546893aa58b9a5fc41141cbe18e3293
Sushrut Patil <sushrut7898@gmail.com>
2020-08-14 12:24:32 +0530

    last

--- excel.py
+++ excel.py
@@ -4,3 +4,14 @@ import openpyxl
 wb = openpyxl.load_workbook("transactions.xlsx")
 
 print(wb.sheetnames)
+
+sheet = wb["Sheet1"]
+
+#wb.create_sheet("Sheet2", 0)
+# wb.remove_sheet(sheet)
+
+cell = sheet["a1"]
+#print(cell.value)
+#print(cell.row)
+
+cell = sheet.cell(row = 1, column = 1)
